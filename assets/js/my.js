d7f76a44dcc44be0e39df7a8bff36a9fb34b4fbc
sharad1126 <sharad.agarwal2014@vit.ac.in>
2016-09-12 03:56:22 +0530

    minor changes

---
+++ assets/js/my.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+  	mode: 'fade',
+  });
+});
+
+var amountScrolled = 300;
+
+$(window).scroll(function() {
+	if ( $(window).scrollTop() > amountScrolled ) {
+		$('a.back-to-top').fadeIn('slow');
+	} else {
+		$('a.back-to-top').fadeOut('slow');
+	}
+});
+
+$('a.back-to-top').click(function() {
+	$('html, body').animate({
+		scrollTop: 0
+	}, 700);
+	return false;
+});
